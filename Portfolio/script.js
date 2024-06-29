a4e9e263400b0a4888660d6410018db5be9dbb87
raunak581 <raunakdubey681@gmail.com>
2024-06-29 23:52:58 +0530

    done

---
+++ Portfolio/script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".bug-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
